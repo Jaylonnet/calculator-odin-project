82596355d8497405e7a23eab873ef5f5d660c3af
Jaylon <jaylonwingfield10@gmail.com>
2022-04-06 16:22:55 +0100

    Added event listener for operation buttons which calls the operate function

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
-let value1 = null;
-let value2 = null;
+let value1 = NaN;
+let value2 = NaN;
+let result = NaN;
 let operator = '';
 
 function add(n1, n2) {
@@ -40,12 +41,28 @@ const numbers = document.querySelectorAll('.number');
 numbers.forEach((number) => {
     number.addEventListener('click', (e) => {
         updateSolution(number.textContent);
+        placeholder.textContent = '';
     });
 });
 
+let placeholder = document.querySelector('#placeholder-value');
 const operations = document.querySelectorAll('.operation');
 operations.forEach((operation) => {
     operation.addEventListener('click', (e) => {
-        operator = operation.textContent;
+        if (!value1 && solution.textContent) {
+            value1 = Number(solution.textContent);
+            operator = operation.textContent;
+            solution.textContent = '';
+            placeholder.textContent = value1;
+        } else if (value1 && solution.textContent) {
+            value2 = Number(solution.textContent);
+            result = operate(operator, value1, value2)
+            value1 = result;
+            value2 = NaN;
+            result = NaN;
+            placeholder.textContent = value1;
+            solution.textContent = '';
+            operator = operation.textContent;
+        };
     });
 });
